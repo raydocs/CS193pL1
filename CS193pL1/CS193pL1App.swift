ee5f0fe7dd1062ddcfa489249705dba772d5f120
ruiruiwan8 <ruiruiwan@accorz.com>
2024-05-22 00:39:35 -0600

    Initial Commit

---
+++ CS193pL1/CS193pL1App.swift
@@ -0,0 +1,17 @@
+//
+//  CS193pL1App.swift
+//  CS193pL1
+//
+//  Created by Ruirui Wan on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CS193pL1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193pL1/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  CS193pL1
+//
+//  Created by Ruirui Wan on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+        }
+        .padding()
+        .foregroundStyle(.orange)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+        
+    }
+}
+#Preview {
+    ContentView()
+}
